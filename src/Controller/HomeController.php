bffeb4178df8b340442d982a5980d76fc2eab67a
Vadim Semeniuc <solomonso@yandex.com>
2025-03-28 16:54:34 +0200

    fix: test DI

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,16 +5,15 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Kernel\Controller\Controller;
+use App\Kernel\DI\ServiceLocator;
 use App\Service\HomeService;
 
 class HomeController extends Controller
 {
     public function index(): void
     {
-        $clasName = HomeService::class;
-
         /** @var HomeService $homeService */
-        $homeService = $this->get($clasName);
+        $homeService = ServiceLocator::getInstance()->get(HomeService::class);
         $this->view('home');
     }
 }
